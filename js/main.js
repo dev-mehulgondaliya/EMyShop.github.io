48f0530c29b2c136038918228f95355906bd3d09
Mehul Gondaliya <137489580+dev-mehulgondaliya@users.noreply.github.com>
2023-08-02 13:41:56 +0530

    create special, testimonial,  contact, footer section and finaly create full e-commerce site

--- js/main.js
+++ js/main.js
@@ -1,5 +1,49 @@
 // special counter
+let daysItem = document.querySelector("#days");
+let hoursItem = document.querySelector("#hours");
+let minItem = document.querySelector("#min");
+let secItem = document.querySelector("#sec");
+
+let countdown = () => {
+    let futureDate = new Date("30 August 2023");
+    let currentDate = new Date();
+    let myDate = futureDate - currentDate;
+
+    let days = Math.floor(myDate / 1000 / 60 / 60 /24);
+    let hours = Math.floor(myDate / 1000 / 60 / 60) % 24;
+    let min = Math.floor(myDate / 1000 / 60 ) % 60;
+    let sec = Math.floor(myDate / 1000 ) % 60;
+
+    daysItem.innerHTML = days;
+    hoursItem.innerHTML = hours;
+    minItem.innerHTML = min;
+    secItem.innerHTML = sec;
+}
+
+countdown();
+
+setInterval(countdown,1000);
 
 // scroll back to top
 
+function scrollTopBack(){
+    let scrollTopBtn = document.querySelector("#scrollUp");
+    window.onscroll = function (){
+        var scroll = document.documentElement.scrollTop;
+        if(scroll >= 250){
+            scrollTopBtn.classList.add('scrollActive');
+        }else{
+            scrollTopBtn.classList.remove('scrollActive');
+        }
+    }
+}
+scrollTopBack();
+
 // nav hide
+let navbar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navbar.forEach(function(a){
+    a.addEventListener("click",function(){
+        navCollapse.classList.remove("show");
+    })
+})
